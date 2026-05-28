ba294b39f3f110fdbe4c5f1bc030b1c651f05acf
ALUNO K10 <alunok10@unifor.local>
2026-05-28 10:28:52 -0300

    first commit in class

---
+++ front-end/src/front_end/core.clj
@@ -0,0 +1,16 @@
+(ns front-end.core 
+  (:gen-class)) 
+
+(defn -main 
+  "I don't do a whole lot ... yet." 
+  [& args] 
+  (println "Escolha uma opcao") 
+  (println "--------------------------------")
+  (println "1 - Cadastrar/consultar dados pessoais")
+  (println "2 - Registrar consumo de alimento ")
+  (println "3 - Registrar realização de atividade física")
+  (println "4 - Consultar extrato de transações")
+  (println "5 - Consultar saldo de calorias")
+
+  
+  )
---
+++ front-end/test/front_end/core_test.clj
@@ -0,0 +1,7 @@
+(ns front-end.core-test
+  (:require [clojure.test :refer :all]
+            [front-end.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
